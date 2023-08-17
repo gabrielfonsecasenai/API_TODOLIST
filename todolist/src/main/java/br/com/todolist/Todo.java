2f2895c17d09e4684ee4223512a6d4248c2a9dce
gabrielfonsecasenai <gabriel.conceicao4@aluno.senai.br>
2023-08-17 16:51:58 -0300

    Configurando Git e criando endpoints

--- todolist/src/main/java/br/com/todolist/Todo.java
+++ todolist/src/main/java/br/com/todolist/Todo.java
@@ -18,4 +18,44 @@ public class Todo {
     @Column(nullable = false)
     private StatusEnum status = StatusEnum.NOT_STARTED;
 
+    public Todo() {
+    }
+
+    public Todo(String title, String description, StatusEnum status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
 }
--- todolist/src/main/java/br/com/todolist/TodolistApplication.java
+++ todolist/src/main/java/br/com/todolist/TodolistApplication.java
@@ -1,7 +1,9 @@
 package br.com.todolist;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodolistApplication {
@@ -10,4 +12,19 @@ public class TodolistApplication {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner criarTarefasFake(TodoRepository todoRpository) {
+		return args -> {
+			todoRpository.deleteAll();
+
+			Todo todo1 = new Todo("Configurar JDK", "Preciso aprender a configurar o jdk corretamente", StatusEnum.NOT_STARTED);
+			Todo todo2 = new Todo("Estudar Java", "Estudar Java para aprender programação", StatusEnum.IN_PROGRESS);
+			Todo todo3 = new Todo("Estudar HTML", "Estudar HTML para aprender programação", StatusEnum.FINISHED);
+
+			todoRpository.save(todo1);
+			todoRpository.save(todo2);
+			todoRpository.save(todo3);
+		};
+	}
 }
+
